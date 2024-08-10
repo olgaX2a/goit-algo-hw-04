8e7d289fbe998fd226442e3655e9bd201f4e7c24
Olga Sekretaryova <sekret.o2a@gmail.com>
2024-08-11 01:09:42 +0300

    add encoding and error handling

--- task-1.py
+++ task-1.py
@@ -1,15 +1,24 @@
+from pathlib import Path
+
 def total_salary(path):
-    with open(path, "r") as sd:
-        lines = [el.strip() for el in sd.readlines()]
+    file_path = Path(path)
 
-        employees_amount = len(lines)
-        
-        total = 0
+    total = 0
+    average = 0
 
-        for line in lines:
-            salary_data = line.split(',')
-            total = total + int(salary_data[1]) 
-        
-        average = total / employees_amount
+    if(file_path.exists()):
+        with open(path, "r", encoding='utf-8') as sd:
+            lines = [el.strip() for el in sd.readlines()]
 
-        return (total, average)
+            employees_amount = len(lines)
+
+            for line in lines:
+                salary_data = line.split(',')
+                total = total + int(salary_data[1]) 
+            
+            average = total / employees_amount
+    
+    else: 
+        print('Provided path does not exist')
+    
+    return (total, average)
--- task2.py
+++ task2.py
@@ -1,12 +1,24 @@
+from pathlib import Path
+
 def get_cats_info(path):
-    with open(path, "r") as sd:
-        lines = [el.strip() for el in sd.readlines()]
+    file_path = Path(path)
 
-        cats = list()
+    cats = list()
 
-        for line in lines:
-            cat_data = line.split(',')
-            cat = {"id": cat_data[0], "name": cat_data[1], "age": cat_data[2]}
-            cats.append(cat)
+    if file_path.exists():
+        with open(path, "r", encoding='utf-8') as sd:
+            lines = [el.strip() for el in sd.readlines()]
 
-        return cats
+
+
+            for line in lines:
+                cat_data = line.split(',')
+                cat = {"id": cat_data[0], "name": cat_data[1], "age": cat_data[2]}
+                cats.append(cat)
+
+            return cats
+        
+    else:
+        print('Provided path does not exist')
+
+    return cats
